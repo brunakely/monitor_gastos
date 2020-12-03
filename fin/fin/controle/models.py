3a8a2c8543ca6504d937cb119330a254e297de51
brunakely <brunakely28@gmail.com>
2020-12-03 12:06:28 -0300

    primeira alteracao

---
+++ fin/fin/controle/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class gastos(models.Model):
+    nome = models.CharField(_max_length=30)
+    valor = models.DecimalField(max_digits=5, decimal_places=2)
+    data = models.DateField(auto_now_add=False)
+    observacao = models.TextField()
+
+
