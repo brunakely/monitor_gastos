c0f2e8393eadc782dbfb814eaa6b777528dd9c76
brunakely <brunakely28@gmail.com>
2020-12-09 10:44:23 -0300

    inclusao do lista.html

--- fin/fin/controle/models.py
+++ fin/fin/controle/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 
 class gastos(models.Model):
-    nome = models.CharField(_max_length=30)
+    nome = models.CharField(max_length=30)
     valor = models.DecimalField(max_digits=5, decimal_places=2)
     data = models.DateField(auto_now_add=False)
     observacao = models.TextField()
